aef577cbfaae4bc3b3209ec3a4e49fe899c44598
dmitry <maslanovdimasik@gmail.com>
2019-01-27 14:16:44 +0300

    master
    Initial project

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/api/v1/test', (req, res) => {
+    res.status(200).send({
+        success: 'true',
+        message: 'todos retrieved successfully',
+        test: 'test'
+    })
+});
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+});
